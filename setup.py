8cd19ee384a68b2e8c338cef72d12bbfca1d3852
Zeke Piskulich <piskulichz@gmail.com>
2022-12-07 08:53:07 -0500

    Updated docs

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pair-distribution-derivatives',
+      version='1.0',
+      description='Pair Distribution Derivatives',
+      author='Zeke Piskulich',
+      author_email='piskulichz@gmail.com',
+      url='https://github.com/piskuliche/pair-distribution-derivatives',
+      packages=['rdfderiv'],
+      
+     )
